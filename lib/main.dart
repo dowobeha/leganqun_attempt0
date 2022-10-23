fc870bb8c80015fcb85608c673b3ba3367bc5173
Lane Schwartz <dowobeha@gmail.com>
2022-10-22 19:54:45 -1000

    First attempt at Cupertino style on macOS and iOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 void main() {
   runApp(const LeganqunApp());
@@ -10,25 +12,24 @@ class LeganqunApp extends StatelessWidget {
   // This widget is the root of the Leganqun app
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Leganqun',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const LeganqunHomePage(title: 'Leganqun Home Page'),
-    );
+    if (UniversalPlatform.isIOS | UniversalPlatform.isMacOS) {
+      return CupertinoApp(
+        title: 'Leganqun',
+        theme: CupertinoThemeData(brightness: Brightness.dark),
+        home: const LeganqunHomePage(title: 'Leganqun Home Page'),
+      );
+    } else {
+      return MaterialApp(
+        title: 'Leganqun',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: const LeganqunHomePage(title: 'Leganqun Home Page'),
+      );
+    }
   }
 }
 
 class LeganqunHomePage extends StatefulWidget {
   const LeganqunHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -39,66 +40,67 @@ class LeganqunHomePage extends StatefulWidget {
 class _LeganqunHomePageState extends State<LeganqunHomePage> {
   int _counter = 0;
 
+  static const definition = "leganqun\n"
+      "лыганқун\n"
+      "/lə.'ɣɑn.qun/"
+      "   — good job! (p.226, *Badten et al, 2008*)";
+
+  FloatingActionButton incrementButton() {
+    return FloatingActionButton(
+      onPressed: _incrementCounter,
+      tooltip: 'Increment',
+      child: const Icon(Icons.add),
+    );
+  }
+
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the ®HomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              '**leganqun** *лыганқун* /lə.\'ɣɑn.qun/ — good job! (p.226, *Badten et al, 2008*)',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    if (UniversalPlatform.isIOS | UniversalPlatform.isMacOS) {
+      return CupertinoPageScaffold(
+        // Uncomment to change the background color
+        // backgroundColor: CupertinoColors.systemPink,
+        navigationBar: const CupertinoNavigationBar(
+          middle: Text("Leganqun"),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Text(definition + "\n"
+                    'You have pressed the button $_counter times.'),
+              ),
+              const SizedBox(height: 20.0),
+              Center(
+                child: CupertinoButton.filled(
+                  onPressed: () => setState(() => _counter++),
+                  child: const Icon(CupertinoIcons.add),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+        appBar: AppBar(title: Text(widget.title)),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(definition),
+              Text('$_counter', style: Theme.of(context).textTheme.headline4)
+            ],
+          ),
+        ),
+        floatingActionButton: incrementButton(), // This trailing comma makes auto-formatting nicer for build methods.
+      );
+    }
   }
 }
